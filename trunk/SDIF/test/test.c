b53c8396ca6f24c52e9d057b1f7303768284734b
virolle <virolle@f0edb446-0c2a-49e4-ba95-239725f1d53d>
1998-11-29 11:44:59 +0000

    *** empty log message ***

--- trunk/SDIF/test/test.c
+++ trunk/SDIF/test/test.c
@@ -10,30 +10,88 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <io.h>
 #include <string.h>
 
 
 
 int
-main(int argc, char *argv[])
+main(int argc, char **argv)
 {
+  char *OutF = NULL;
+  char *SdifTypesFile = NULL;
+
   SdifFileT *SdifF;
-  float t[10] = { 1., 2., 3., 4., 5., 6., 7., 8., 9., 0.};
+  SdifFileModeET openMode;
+
+
+  /* test var */
+  float t[8] = { 1., 2., 3., 4., 5., 6., 7.};
+
+
+/* argument configuration (Outfilename, openmode, UserTypesFile) */
+  if (argc > 1)
+  {
+	OutF = SdifCreateStrNCpy(argv[1], SdifStrLen(argv[1])+1);
+	if (argc > 2)
+	{
+	  openMode = atoi(argv[2]);
+	  if (argc > 3)
+	    SdifTypesFile = SdifCreateStrNCpy(argv[3], SdifStrLen(argv[3])+1); 
+	  else
+		SdifTypesFile = SdifCreateStrNCpy("", SdifStrLen("")+1); 
+	}
+	else
+	  openMode = eWriteFile;
+  }
+  else
+    OutF = SdifCreateStrNCpy("stdout", SdifStrLen("stdout")+1);
 
 
 
+  SdifPrintVersion();
+  SdifGenInit(SdifTypesFile);
+ 
 
-  SdifGenInit("SdifTypes.STYP");
-  SdifF = SdifOpenFile("test.sdif", eWriteFile);
-  SdifPrintFrameHeader   (stderr, SdifSetCurrFrameHeader (SdifF, 'fram', _SdifUnknownSize, 1, 0, 1.234));
-  SdifPrintMatrixHeader  (stderr, SdifSetCurrMatrixHeader(SdifF, 'mtrx', eFloat4, 1, 10));
-  SdifPrintOneRow        (stderr, SdifSetCurrOneRow      (SdifF, t));
-  SdifPrintOneRow        (stderr, SdifSetCurrOneRowCol (SdifF, 5, 10.));
-  SdifPrintOneRow        (stderr, SdifSetCurrOneRowCol (SdifF, 10, 10.));
-  SdifPrintOneRow        (stderr, SdifSetCurrOneRowCol (SdifF, 10, SdifGetCurrOneRowCol (SdifF, 1)));
-  /*SdifPrintAllType(stderr, gSdifPredefinedTypes);*/
-  SdifCloseFile(SdifF);
+  SdifF = SdifFOpen(OutF, eWriteFile);
+  SdifFOpenText(SdifF, "stderr", eWriteFile);
+
+  SdifFSetCurrFrameHeader (SdifF, 'fram', _SdifUnknownSize, 1, 0, 1.234);
+  SdifFWriteFrameHeader  (SdifF);
+
+  SdifFSetCurrMatrixHeader(SdifF, 'mtrx', eFloat4, 1, 7);
+  SdifFWriteMatrixHeader (SdifF);
+
+  fprintf(stderr, "\n");
+  SdifFPrintFrameHeader  (SdifF);
+  SdifFPrintMatrixHeader (SdifF);
+
+  SdifFSetCurrOneRow    (SdifF, t);
+  SdifFPrintOneRow      (SdifF);
+  SdifFWriteOneRow      (SdifF);
+  SdifFSetCurrOneRowCol (SdifF, 4, 10);
+  SdifFPrintOneRow      (SdifF);
+  SdifFWriteOneRow      (SdifF);
+  SdifFSetCurrOneRowCol (SdifF, 7, 10);
+  SdifFPrintOneRow      (SdifF);
+  SdifFWriteOneRow      (SdifF);
+  SdifFSetCurrOneRowCol (SdifF, 7, SdifFCurrOneRowCol (SdifF, 1));
+  SdifFPrintOneRow      (SdifF);
+  SdifFWriteOneRow      (SdifF);
+
+  
+  SdifFClose(SdifF);
+
+  
+  SdifPrintAllType(stderr, gSdifPredefinedTypes);
+
+
   SdifGenKill();
 
+
+  free(OutF);
+  free(SdifTypesFile);
+  fprintf(stdout, "%e\n", _Sdif_MIN_DOUBLE_);
   return 0;
 }
