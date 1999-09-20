78771a0c1008a78f9fc0064593ef0dd7aa54de9c
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
1999-09-20 13:23:09 +0000

    First finished version, API to be improved.

--- SDIF/sdif/SdifSelect.h
+++ SDIF/sdif/SdifSelect.h
@@ -1,4 +1,4 @@
-/* $Id: SdifSelect.h,v 3.1 1999-08-31 10:03:00 schwarz Exp $
+/* $Id: SdifSelect.h,v 3.2 1999-09-20 13:23:03 schwarz Exp $
  *
  *               Copyright (c) 1998 by IRCAM - Centre Pompidou
  *                          All rights reserved.
@@ -55,54 +55,99 @@ DESCRIPTION
   Numerical specifications can also be ranges l-u (a missing value l or u 
   would be replaced by the respective min/max).
   
-  - Whitespace between the specs should be allowed.
+  - Whitespace between the specs is allowed (before the :: it is
+    considered part of the filename).
+
+  - Again, watch out for possible ambiguities between floating point
+    time spec and column spec: @1.2 will be time 1.2, not time 1,
+    column 2.  It is advisable to put the time spec last to avoid this
+    (.2@1 is ok) or use spaces (@1 .2 is also ok.
   
+TODO
+  - handle quoted strings:
   - Allow quoting names for weird frame and matrix types.
-  
   - One could later allow regular expressions for names. 
+  - functions to set up specs (lists of ranges)
 
 LOG
   $Log: not supported by cvs2svn $
-
-*/
+  Revision 3.1  1999/08/31  10:03:00  schwarz
+  Added module SdifSelect which parses an access specification to a
+  chosen part of SDIF data.  Can be added to a file name.  */
 
 
 #ifndef _SdifSelect_
 #define _SdifSelect_
 
 
+#include "SdifHard_OS.h"	/* SdifSignature */
+#include "SdifList.h"
+
+
 /* tokens (numerical ids) for sdifspec separators */
 typedef enum { sst_specsep, sst_stream, sst_frame, sst_matrix, sst_column, 
 	       sst_row,     sst_time,   sst_list,  sst_range,  sst_delta,
-	       sst_num	/* number of tokens */
+	       sst_num,	/* number of tokens */	   sst_norange = 0
 } SdifSelectTokens;
 
 
+
 /*DOC: 
-  one value of different possible types in a selection element
-  (the element specifies which type is actually used).  
+  Selection element interface (returned by SdifGetNextSelection*):
+  One basic data element value, with optional range.  
+  The meaning of range is determined by rangetype: 
+
+  [] 0		no range
+  [] sst_range	range is value..range
+  [] sst_delta	range is value-range..value+range
+*/
+
+typedef struct 
+{
+    int		       value, range;
+    SdifSelectTokens   rangetype; /* 0 for not present, sst_range, sst_delta */
+} SdifSelectElementIntT;
+
+typedef struct 
+{
+    double	       value, range;
+    SdifSelectTokens   rangetype; /* 0 for not present, sst_range, sst_delta */
+} SdifSelectElementRealT;
+
+/* no SdifSelectElementSignatureT or string range, since it makes no sense */
+
+
+
+/*DOC:
+  Internal: one value of different possible types in a selection
+  element (the element list determines which type is actually used).  
 */
 typedef union SdifSelectValueS 
 {
-    int		   integer;
-    double	   real;
+    int            integer;
+    double         real;
+    char	   *string;
     SdifSignature  signature;
 } SdifSelectValueT;
 
-
 /*DOC: 
-  selection element: one basic data element, 
-  with optional range (this is lower, range is upper bound) 
- */
+  Selection element internal data structure:
+  One basic data element, with optional <ul>
+  <li> range (value is lower, range is upper bound) or 
+  <li> delta (value-range is lower, value+range is upper bound)
+  </ul>
+*/
 typedef struct SdifSelectElementS
 {
     SdifSelectValueT value;
     SdifSelectValueT range;
-    SdifSelectTokens rangetype;	/* 0 for not present, sst_range, sst_delta */
+    SdifSelectTokens rangetype; /* 0 for not present, sst_range, sst_delta */
 } SdifSelectElementT, *SdifSelectElementP;
 
-
-/*DOC: */
+/*DOC: 
+  Holds a selection of what data to access in an SDIF file,
+  parsed from a simple regular expression.  
+*/
 typedef struct
 {
     char	*filename,	/* allocated / freed by 
@@ -114,19 +159,187 @@ typedef struct
 
 
 
+/* 
+// FUNCTION GROUP:	Init/Deinit
+ */
+
 /* init module, called by SdifGenInit */
 int SdifInitSelect (void);
 
-int SdifInitSelection (SdifSelectionT *sel, char *filename, int namelen);
+/*DOC: 
+*/
+int SdifInitSelection (SdifSelectionT *sel, const char *filename, int namelen);
+
+/*DOC: 
+*/
 int SdifFreeSelection (SdifSelectionT *sel);
 
-char *SdifGetFilenameAndSelection (/*in*/  char *filename, 
+/*
+// FUNCTION GROUP:	Parse and Set Selection
+*/
+
+
+/*DOC: 
+  Returns pointer to first char of select spec (starting with ::), 
+  or NULL if not found.
+*/
+char *SdifSelectFindSelection (const char *filename);
+
+
+/*DOC: 
+*/
+char *SdifGetFilenameAndSelection (/*in*/  const char *filename, 
 				   /*out*/ SdifSelectionT *sel);
 
-int		SdifSelectGetNextInt (SdifListP list);
-double		SdifSelectGetNextDouble (SdifListP list);
-SdifSignature	SdifSelectGetNextSignature (SdifListP list);
+/*DOC: 
+*/
+void SdifPrintSelection (FILE *out, SdifSelectionT *sel, int options);
 
+
+
+/*
+// FUNCTION GROUP:	Add Selections to Element Lists
+*/
+
+/* Give documentation and fake prototype for _add... macro generated functions.
+   Cocoon ignores the #if 0.
+*/
+#if 0
+
+/*DOC:
+  Create and add one value to selection element list.  There are four 
+  functions generated automatically, with the meta type-variables _type_ and 
+  _datatype_:
+  [] _type_ is one of:  <br> Int, Real,   Signature,     String, for
+  [] _datatype_ of:	<br> int, double, SdifSignature, char *, respectively.
+*/
+SdifSelectAdd_type_ (SdifListT *list, _datatype_ value);
+
+/*DOC:
+  Create and add one range to selection element list.  There are four 
+  functions generated automatically, with the meta type-variables _type_ and 
+  _datatype_:
+  [] _type_ is one of:  <br> Int, Real,   Signature,     String, for
+  [] _datatype_ of:	<br> int, double, SdifSignature, char *, respectively.
+*/
+SdifSelectAdd_type_Range (SdifListT *list, 
+			  _datatype_ value, 
+			  SdifSelectTokens rt, 
+			  _datatype_type range);
+
+#endif
+
+#ifdef _ANSI_C_SOURCE
+
+#define _addrangeproto(name, type, field) \
+void SdifSelectAdd##name##Range (SdifListT *list, \
+				 type value, SdifSelectTokens rt, type range)
+
+#define _addsimpleproto(name, type, field) \
+void SdifSelectAdd##name (SdifListT *list, type value)
+
+#define _addproto(name, type, field) \
+_addsimpleproto (name, type, field); \
+_addrangeproto  (name, type, field);
+
+_addproto (Int,	      int,		integer)
+_addproto (Real,      double,		real)
+_addproto (Signature, SdifSignature,	signature)
+_addproto (String,    char *,		string)
+
+#else
+
+void SdifSelectAddInt (SdifListT *list, int value) ; 
+void SdifSelectAddIntRange (SdifListT *list, int value, SdifSelectTokens rt, int range) ; 
+void SdifSelectAddReal (SdifListT *list, double value) ; 
+void SdifSelectAddRealRange (SdifListT *list,                                   double value, SdifSelectTokens rt, double range) ; 
+void SdifSelectAddSignature (SdifListT *list, SdifSignature value) ; 
+void SdifSelectAddSignatureRange (SdifListT *list, SdifSignature value, SdifSelectTokens rt,   SdifSignature range) ; 
+void SdifSelectAddString (SdifListT *list, char * value) ; 
+void SdifSelectAddStringRange (SdifListT *list, char * value, SdifSelectTokens rt,      char * range) ; 
+
+#endif
+
+
+/*
+// FUNCTION GROUP:	Query parsed ranges (list of ranges).
+*/
+
+/*DOC:
+  Query parsed ranges (list of ranges) for a selection element (one of
+  the SdifListP lists in SdifSelectionT).  Init list traversal with
+  SdifListInitLoop, then call SdifSelectGetNext<type>(list) until it
+  returns 0.
+
+  The number of selections in the list is SdifListGetNbData(list), if
+  it is 0, or SdifListIsEmpty(list) is true, then there was no
+  selection for that element.
+
+  If force_range is 1, the out value is converted to a range in any
+  case, with value <= range guaranteed.  
+*/
+int SdifSelectGetNextInt  (/*in*/  SdifListP list, 
+			   /*out*/ SdifSelectElementIntT  *range, 
+			   /*in*/  int force_range);
+
+/*DOC: 
+  See SdifSelectGetNextInt.
+*/
+int SdifSelectGetNextReal (/*in*/  SdifListP list, 
+			   /*out*/ SdifSelectElementRealT *range, 
+			   /*in*/  int force_range);
+
+/*DOC: 
+  Query list of parsed selection elements (one of the SdifListP
+  lists in SdifSelectionT).  Init list traversal with
+  SdifListInitLoop, then call SdifSelectGetNext<type>(list) until it
+  returns 0.
+
+  See also SdifSelectGetNextInt.  
+*/
+SdifSignature  SdifSelectGetNextSignature (/*in*/  SdifListP list);
+
+/*DOC: 
+  See SdifSelectGetNextSignature.
+*/
+char	      *SdifSelectGetNextString    (/*in*/  SdifListP list);
+
+
+
+/*
+// FUNCTION GROUP:	Selection Testing Functions
+*/
+
+int SdifSelectTestIntRange (SdifSelectElementT *elem, int cand);
+int SdifSelectTestRealRange (SdifSelectElementT *elem, double cand);
+
+int SdifSelectTestInt (SdifListT *list, int cand);
+int SdifSelectTestReal (SdifListT *list, double cand);
+int SdifSelectTestSignature (SdifListT *list, const SdifSignature cand);
+int SdifSelectTestString (SdifListT *list, const char *cand);
+
+
+
+/*
+// FUNCTION GROUP:	Using a Selection in File I/O.
+*/
+
+#ifndef SdifFileT_
+#define SdifFileT_
+typedef struct SdifFileS SdifFileT;
+#endif
+
+/*DOC:
+  Test the selection elements applicable to frames: time, stream, frame type.
+  Can be called after SdifFReadFrameHeader().
+*/
+int SdifFCurrFrameIsSelected (SdifFileT *file, SdifSelectionT *sel);
+
+/*DOC:
+  Test the selection elements applicable to matrices: the matrix signature
+  Can be called after SdifFReadMatrixHeader().
+*/
+int SdifFCurrMatrixIsSelected (SdifFileT *file, SdifSelectionT *sel);
 
 
 #endif /* _SdifSelect_ */
--- SDIF/sdif/test/testselect.c
+++ SDIF/sdif/test/testselect.c
@@ -1,10 +1,14 @@
-/* $Id: testselect.c,v 1.1 1999-08-31 10:03:38 schwarz Exp $
+/* $Id: testselect.c,v 1.2 1999-09-20 13:23:09 schwarz Exp $
  *
  * testselect		30. August 1999		Diemo Schwarz
  *
  * Test functions from SdifSelect.c
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  1999/08/31  10:03:38  schwarz
+ * Added test code for module SdifSelect which parses an access specification to a
+ * chosen part of SDIF data.  Can be added to a file name.
+ *
  */
 
 #include <stdio.h>
@@ -13,18 +17,46 @@
 
 main (int argc, char *argv [])
 {
-  char *arg = argc > 1  ?  argv [1]  :  NULL;
-  SdifSelectionT sel;
+    SdifSelectionT		sel;
+    SdifSelectElementIntT	intrange;
+    SdifSelectElementRealT	realrange;
+    SdifSignature		sig;
+    char			*arg = argc > 1  ?  argv [1]  :  NULL;
+    
+    printf ("%s %s\n", argv [0], arg);
+    SdifGenInit ("");
+    
+    SdifGetFilenameAndSelection (arg, &sel);
+    
+    printf ("selection: file %s  basename %s", sel.filename, sel.basename);
+    
+    SdifListInitLoop (sel.stream);
+    while (SdifSelectGetNextInt (sel.stream, &intrange, 1))
+	printf ("\n  stream\t%d - %d ", intrange.value, intrange.range);
 
-  printf ("%s %s\n", argv [0], arg);
-  SdifGenInit ("");
+    SdifListInitLoop (sel.frame);
+    while (sig = SdifSelectGetNextSignature (sel.frame))
+	printf ("\n  frame\t'%s' ", SdifSignatureToString (sig));
 
-  SdifGetFilenameAndSelection (arg, &sel);
-  printf ("selection: file %s  basename %s\nstream %d  frame %s  time %f\n",
-	  sel.filename, sel.basename,
-	  SdifSelectGetNextInt (sel.stream),
-	  SdifSignatureToString (SdifSelectGetNextSignature (sel.frame)),
-	  SdifSelectGetNextDouble (sel.time));
+    SdifListInitLoop (sel.matrix);
+    while (sig = SdifSelectGetNextSignature (sel.matrix))
+	printf ("\n  matrix\t'%s' ", SdifSignatureToString (sig));
+
+    SdifListInitLoop (sel.column);
+    while (SdifSelectGetNextInt (sel.column, &intrange, 1))
+	printf ("\n  column\t%d - %d ", intrange.value, intrange.range);
+
+    SdifListInitLoop (sel.row);
+    while (SdifSelectGetNextInt (sel.row, &intrange, 1))
+	printf ("\n  row\t%d - %d ", intrange.value, intrange.range);
+
+    SdifListInitLoop (sel.time);
+    while (SdifSelectGetNextReal (sel.time, &realrange, 1))
+	printf ("\n  time\t%f - %f ", realrange.value, realrange.range);
+
+    printf ("\n\n");
+
+    SdifPrintSelection (stdout, &sel, 0);
 	
-  SdifGenKill ();
+    SdifGenKill ();
 }
