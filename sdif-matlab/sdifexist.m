22e4f48f3260ce7e1be1f23ec8ed4295018bcdd9
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2000-05-12 14:03:54 +0000

    Oops-style errors.

--- sdif-matlab/sdifexist.m
+++ sdif-matlab/sdifexist.m
@@ -3,12 +3,13 @@
 %          sdifexist(name)	Exits with error if file's not there.
 % result = sdifexist(name)	Returns flag.
 
-% $Id: sdifexist.m,v 1.1 2000-05-04 13:24:08 schwarz Exp $
+% $Id: sdifexist.m,v 1.2 2000-05-12 14:03:52 schwarz Exp $
 %
 % sdifexist.m	4. May 2000	Diemo Schwarz
 %
 % $Log: not supported by cvs2svn $
-
+% Revision 1.1  2000/05/04  13:24:08  schwarz
+% Matlab mex extension and support functions to load SDIF files.
 
 function result = sdifexist (name)
     result = 1;
