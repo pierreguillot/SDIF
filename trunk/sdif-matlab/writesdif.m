5d605a9d60889a8e2fa0540850793ed6d65aa28d
tisseran <tisseran@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2000-05-15 13:11:59 +0000

    Corrected comments style

--- trunk/sdif-matlab/writesdif.m
+++ trunk/sdif-matlab/writesdif.m
@@ -18,15 +18,22 @@
 %
 
 %
-% $Id: writesdif.m,v 1.2 2000-05-15 13:07:49 tisseran Exp $
+% $Id: writesdif.m,v 1.3 2000-05-15 13:11:59 tisseran Exp $
 %
 % writesdif.m	12. May 2000	Patrice Tisserand
 %
 % $Log: not supported by cvs2svn $
-%# Revision 1.1  2000/05/12  16:14:15  tisseran
-%# Mexfile to write sdif files in matlab.
-%# TODO: add possibility to use several file at same time.
-%#       add test on arguments
-%#
+% Revision 1.2  2000/05/15  13:07:49  tisseran
+% Added test for input arguments:
+%    Is Matrix Signature a char?
+% Removed debugging message (mexPrintf("Matrix Loop") ...)
+% Removed unused variable
+% Change comment style for writesdif.m
+%
+% Revision 1.1  2000/05/12  16:14:15  tisseran
+% Mexfile to write sdif files in matlab.
+% TODO: add possibility to use several file at same time.
+%       add test on arguments
+%
 %
 % Matlab mex extension and support functions to write SDIF files.
