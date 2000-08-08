d80e8fa5403143e9faaf99074f61c76f134ab8d5
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2000-08-08 15:59:10 +0000

    SDIF-TDL version 0.2 (no matrix:role attribute, types-version, types-revision. Generation details in STYP 1NVT / HTML footer.

--- trunk/SDIF/types/xmltostyp.pl
+++ trunk/SDIF/types/xmltostyp.pl
@@ -1,9 +1,29 @@
 #!/usr/bin/perl
+#
+# $Id: xmltostyp.pl,v 1.2 2000-08-08 15:59:10 schwarz Exp $
+#
+# xmltostyp.pl		6. July 2000		Diemo Schwarz
+#
+# Translate SDIF types description in XML to STYP format for the SDIF-library.
+#
+# TODO:
+# - use DOM parser instead of XML::Node
+#
+# $Log: not supported by cvs2svn $
+# revision 1.1 date: 2000/07/27 13:32:03;  author: schwarz;
+# First preliminary trial test version of SDIF types definition using XML:
+# xmltostyp.pl generates library-types file sdiftypes.styp from sdiftypes.xml,
+# xmltohtml.pl generates documentation file sdiftypes.html from sdiftypes.xml.
 
 
+use English;
 use XML::Node;
 
-my $version    = '';
+
+my $cvsrev     = '$Id: xmltostyp.pl,v 1.2 2000-08-08 15:59:10 schwarz Exp $ ';
+my $tdlversion = '';
+my $version    = 'unknown';
+my $revision   = '';
 my $framesig   = '';
 my $matrixsig  = '';
 my $matrixname = '';
@@ -19,17 +39,19 @@ $xml = new XML::Node;
 
 
 # header handler
-$xml->register (">sdif-tdl", start => \&header);
-$xml->register (">sdif-tdl:types-version", attr => \$version);
-$xml->register (">sdif-tdl", end   => sub { print "}\n"; } );
+$xml->register (">sdif-tdl:version",	    attr  => \$tdlversion);
+$xml->register (">sdif-tdl",		    end   => sub { print "}\n"; } );
+$xml->register (">sdif-tdl>types-version",  char  => \$version);
+$xml->register (">sdif-tdl>types-revision", char  => \$revision);
 
 # column handlers
 $xml->register ("column:name", attr => sub { push @columns, $_[1] });
 
 # matrix handlers, free or in frame
-$xml->register ("matrix:signature", attr => \$matrixsig);
-$xml->register ("matrix:name",      attr => \$matrixname);
-$xml->register ("matrix",	    end  => \&matrix);
+$xml->register ("matrix",	    start => \&header);
+$xml->register ("matrix:signature", attr  => \$matrixsig);
+$xml->register ("matrix:name",      attr  => \$matrixname);
+$xml->register ("matrix",	    end   => \&matrix);
 
 # matrix in frame
 $xml->register (">sdif-tdl>frame>matrix:signature",    attr  => \&addmatrix);
@@ -37,14 +59,16 @@ $xml->register (">sdif-tdl>frame>matrixref:signature", attr  => \&addmatrix);
 # todo: heed roles
 
 # frame handlers
-$xml->register (">sdif-tdl>frame:signature", attr => \$framesig);
-$xml->register (">sdif-tdl>frame",	     end  => \&frame);
+$xml->register (">sdif-tdl>frame",	     start => \&header);
+$xml->register (">sdif-tdl>frame:signature", attr  => \$framesig);
+$xml->register (">sdif-tdl>frame",	     end   => \&frame);
 
 
 # process all input files
-for (@ARGV)
+local $arg;
+for $arg (@ARGV)
 {
-    $xml->parsefile ($_);
+    $xml->parsefile ($arg);
 }
 
 # end of main
@@ -52,10 +76,33 @@ for (@ARGV)
 
 
 sub header
-{
-    print "SDIF\n\n";
-    $version  &&  print "1NVT\n{\n  SdifTypesVersion $version;\n}\n\n";
-    print "1TYP\n{\n";
+{   # write header only once!
+    if (defined $version)
+    {
+	my @g = stat $PROGRAM_NAME;
+	my @s = stat $arg;
+	my %nvt = (SdifTypesVersion   => $version,
+		   GenerationDate     => scalar localtime,
+		   GenerationUser     => $ENV{USER},
+		   GenerationHost     => $ENV{HOST},
+		   GenerationDir      => $ENV{PWD},
+		   Generator	      => $PROGRAM_NAME,
+		   GeneratorFiledate  => scalar localtime($g[9]),
+		   GeneratorRevision  => $cvsrev,
+		   SourceFile	      => $arg,
+		   SourceFiledate     => scalar localtime($s[9]),
+		   SourceFileRevision => $revision);
+
+	print "SDIF\n\n1NVT\n{\n";
+	for my $name (sort keys %nvt)
+	{
+	    # replace reserved chars
+	    (my $value = $nvt{$name}) =~ tr(.;, \n\t)(_); 
+	    print "  $name\t$value;\n";
+	}
+	print "}\n\n1TYP\n{\n";
+	undef $version;
+    }
 }
 
 
