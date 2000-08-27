7da3049296968e70680fd41719f2f306ce010b21
nobody <nobody@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2000-08-27 14:32:28 +0000

    This commit was manufactured by cvs2svn to create tag 'sdif_matlab_v0_1_5'.

---
+++ tags/sdif_matlab_v0_1_5/sdif-matlab/testl.m
@@ -0,0 +1,52 @@
+
+function testl(name)
+    format short g
+    format loose  
+    %format compact
+
+    if nargin > 0,
+	testfile (name)
+    else
+%	testfile ('test2.sdif')
+%	testfile ('test2.sdif::@0+0.01#0')
+%	testfile ('test2.sdif::@0+0.01#1')
+	testfile ('data/test2.sdif::1TRC/1TRC')
+	testfile ...
+	   ('/snd/dhaes/ADDsequence4seg1/sequence4seg1.energy.sdif::1NRG/INRG')
+    end
+return
+
+
+function testfile (name, types)
+    if nargin == 1,
+	disp (['Testing SDIF file "' name '":']);
+	NVTinfo = loadsdif (name)
+    else
+	disp (['Testing SDIF file "' name '" with types ' types ':']);
+	NVTinfo = loadsdif (name, types)
+    end
+
+    while (1)
+	[ data, time, stream, frame, matrix ] = loadsdif;
+	if isempty (data),  
+	    disp ('eouf!');
+	    disp (' ');
+	    break;
+	else
+	    disp (['time ' num2str(time) '  stream ' num2str(stream) ...
+		   '  frame ' frame '  matrix ' matrix '  data [' ...
+		   num2str(size(data)) '] =']);
+	    disp (data);
+	end
+    end
+    
+    loadsdif ('close')
+return
+
+
+% nvtinfo			        = loadsdif ('open', name[::selection])
+% [       time, stream, frame ]         = loadsdif ('nextframe',  [selection])
+% [ data, time, stream, frame, matrix ] = loadsdif ('nextmatrix', [selection])
+%					  loadsdif ('close')
+% 
+% later:  loadsdif ([file-id], ...)  for multiple files
