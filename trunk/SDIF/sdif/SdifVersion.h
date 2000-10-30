badb73da1803566ee109600faa05bf8a8510c433
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2000-10-30 20:05:16 +0000

    Changed debug option switch from -g to -g2

--- trunk/SDIF/sdif/SdifVersion.h
+++ trunk/SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$Checksum: SRC 1892784492 $ IRCAM $Date: 2000-10-30 19:49:34 $"
+#define AUTOCKSUM "$Checksum: SRC 1892784492 $ IRCAM $Date: 2000-10-30 20:05:14 $"
