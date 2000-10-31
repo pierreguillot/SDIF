988fcbb144f7729bc6200e558ac11b54df506ce2
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2000-10-31 10:57:00 +0000

    Adjusted make file paths such that auto cksum version generation works with VPATH

--- SDIF/sdif/SdifVersion.h
+++ SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$Checksum: SRC 1892784492 $ IRCAM $Date: 2000-10-30 20:05:14 $"
+#define AUTOCKSUM "$Checksum: 261722469 $ IRCAM $date: "__DATE__" $"
