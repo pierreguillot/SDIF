2b24a8506a55552a2164c621eaf733d92fdce535
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2001-04-18 16:24:52 +0000

    Generate ../sdif/sdiftypes.h with xmltostyp.pl to include standard types into the code as #define'd string SDIFTYPES_STRING (--> no need to install SdifTypes.STYP) Generate ../sdif/SdifTypes.STYP directly. Changed usage of xmltostyp.pl, added help.

--- trunk/SDIF/types/xmltostyp.pl
+++ trunk/SDIF/types/xmltostyp.pl
@@ -1,12 +1,15 @@
 #!/usr/bin/perl
 #
-# $Id: xmltostyp.pl,v 1.4 2000-09-27 10:12:47 schwarz Exp $
+# $Id: xmltostyp.pl,v 1.5 2001-04-18 16:24:52 schwarz Exp $
 #
 # xmltostyp.pl		6. July 2000		Diemo Schwarz
 #
 # Translate SDIF types description in XML to STYP format for the SDIF-library.
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.4  2000/09/27 10:12:47  schwarz
+# Ideas for datatype, datarange.
+#
 # Revision 1.3  2000/08/09  14:43:50  schwarz
 # Put all SDIF types into XML format.  Lots of descriptions still missing.
 # Full "about this document" info.
@@ -26,9 +29,14 @@
 
 use English;
 use XML::Node;
+use File::Basename;
+
+# predeclare subs
+sub out;
+sub outtofile;
 
 
-my $cvsrev     = '$Id: xmltostyp.pl,v 1.4 2000-09-27 10:12:47 schwarz Exp $ ';
+my $cvsrev     = '$Id: xmltostyp.pl,v 1.5 2001-04-18 16:24:52 schwarz Exp $ ';
 my $tdlversion = '';
 my $version    = '';
 my $revision   = '';
@@ -48,7 +56,8 @@ $xml = new XML::Node;
 
 # header handler
 $xml->register (">sdif-tdl:version",	    attr  => \$tdlversion);
-$xml->register (">sdif-tdl",		    end   => sub { print "}\n"; } );
+$xml->register (">sdif-tdl",		    end   => 
+		sub { outtofile "}\n"; } );	# end of 1TYP block
 $xml->register (">sdif-tdl>types-version",  char  => \$version);
 $xml->register (">sdif-tdl>types-revision", char  => \$revision);
 
@@ -72,16 +81,74 @@ $xml->register (">sdif-tdl>frame:signature", attr  => \$framesig);
 $xml->register (">sdif-tdl>frame",	     end   => \&frame);
 
 # a little grouping
-$xml->register (">sdif-tdl>section",	     start => sub { print "\n"; } );
+$xml->register (">sdif-tdl>section",	     start => sub { out "\n"; } );
 
 
-
-# process all input files
-local $arg;
-for $arg (@ARGV)
+# arguments
+if (@ARGV < 3)
 {
-    $xml->parsefile ($arg);
+    print "Usage: $PROGRAM_NAME input-sdiftypes.xml output-sdiftypes.styp output-sdiftypes.h\n";
+    exit 1;
 }
+
+local ($xmlin, $stypout, $typesout) = @ARGV;
+my $stypbase  = basename ($stypout);
+my $typesbase = basename ($typesout);
+
+open(STYPOUT, ">$stypout")  ||  die;
+
+# process input file
+local $styp;	# the whole styp file as a string
+$xml->parsefile ($xmlin);
+
+close STYPOUT;
+
+
+# open and prepare types.h
+
+my $comment = "This document $typesout generated " . scalar localtime() . 
+    " by $PROGRAM_NAME from $xmlin";
+
+=pod
+
+    "Generation $ENV{USER} " . scalar localtime() . " $ENV{HOST} $ENV{PWD}"
+    "Generator $PROGRAM_NAME " . scalar localtime($g[9]) . " $cvsrev
+	  ->TH->t("Source file")->TD->t("$xmlin")
+	  ->TD(nowrap)->t(scalar localtime($s[9]))
+	  ->TD(nowrap)->t($revision)->_TR
+      ->TR
+	->TD(colspan => 1, bgcolor => "gray")->FONT(size => $fs)
+	  ->A(href => "../index.html", target => "_parent")
+	  ->B->t("Back")->_B->_A->_FONT
+        ->TD(colspan => 1, bgcolor => "gray")->FONT(size => $fs)
+	  ->A(href => "http://www.ircam.fr/sdif", target => "_parent")
+	  ->B->t("SDIF")->e('nbsp')->t("Home")->_B->_A->_FONT
+        ->TD(colspan => 1, bgcolor => "gray")->FONT(size => $fs)
+	  ->A(href => "http://www.ircam.fr/anasyn", target => "_parent")
+	  ->B->t("Analysis/Synthesis Team")->_B->_A->_FONT
+       ->TD(colspan => 1, bgcolor => "gray")->FONT(size => $fs)
+	  ->A(href => "http://www.ircam.fr", target => "_parent")
+	  ->B->t("IRCAM")"
+
+=cut
+
+# every line in $styp becomes "line\n"\\n
+$styp =~ s((.*?)\n)("$1\\n"\\\n)g;
+
+open (H, ">$typesout")  ||  die;
+print H <<"END_OF_TEXT";
+/* $comment
+*/
+#ifndef _SDIFTYPES_H_
+#define _SDIFTYPES_H_
+
+#define SDIFTYPES_STRING \\
+$styp
+
+#endif
+END_OF_TEXT
+
+close H;
 
 # end of main
 
@@ -92,7 +159,7 @@ sub header
     if (defined $version)
     {
 	my @g = stat $PROGRAM_NAME;
-	my @s = stat $arg;
+	my @s = stat $xmlin;
 	my %nvt = (SdifTypesVersion   => $version,
 		   GenerationDate     => scalar localtime,
 		   GenerationUser     => $ENV{USER},
@@ -101,18 +168,18 @@ sub header
 		   Generator	      => $PROGRAM_NAME,
 		   GeneratorFiledate  => scalar localtime($g[9]),
 		   GeneratorRevision  => $cvsrev,
-		   SourceFile	      => $arg,
+		   SourceFile	      => $xmlin,
 		   SourceFiledate     => scalar localtime($s[9]),
 		   SourceFileRevision => $revision);
 
-	print "SDIF\n\n1NVT\n{\n";
+	outtofile "SDIF\n\n1NVT\n{\n";
 	for my $name (sort keys %nvt)
 	{
 	    # replace reserved chars
 	    (my $value = $nvt{$name}) =~ tr(.:;, \n\t)(_); 
-	    print "  $name\t$value;\n";
+	    outtofile "  $name\t$value;\n";
 	}
-	print "}\n\n1TYP\n{\n";
+	outtofile "}\n\n1TYP\n{\n";	# end of 1NVT block, start of 1TYP
 	undef $version;
     }
 }
@@ -125,7 +192,7 @@ sub addmatrix
 
 sub matrix
 {
-    print "1MTD $matrixsig { ", join (', ', @columns), " }\n";
+    out "1MTD $matrixsig { ", join (', ', @columns), " }\n";
     $matrixnames{$matrixsig} = $matrixname;
     @columns    = ();
     $matrixsig  = ''; 
@@ -134,7 +201,19 @@ sub matrix
 
 sub frame 
 {
-    print "1FTD $framesig { ", map ("$_ $matrixnames{$_}; ", @components), " }\n";
+    out "1FTD $framesig { ", map ("$_ $matrixnames{$_}; ", @components), " }\n";
     @components = ();
     $framesig   = '';
+}
+
+
+sub outtofile
+{
+    print STYPOUT @_;	# .styp file output
+}
+
+sub out
+{
+    outtofile @_;		# standard out -> .styp
+    $styp .= join('', @_);	# types.h to be included
 }
