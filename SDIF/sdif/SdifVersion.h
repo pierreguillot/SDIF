8472cc15e76205279681ce724fdee7c7d7a1d346
tisseran <tisseran@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2001-04-25 11:29:11 +0000

    Change sdif_foralltype macro to compile on MacOS X. Old version: sdif_foralltypes sdif__foralltypes(macro,) New version; sdif_foralltypes sdif__foralltypes(macro, empty_definition) with #define emppty_definition /* empty definition */
    Change AUTHORS to reflect people working on SDIF library

--- SDIF/sdif/SdifVersion.h
+++ SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$SDIFChecksum: 3202968638 $ IRCAM $SDIFdate: "__DATE__" $"
+#define AUTOCKSUM "$SDIFChecksum: 1091498371 $ IRCAM $SDIFdate: "__DATE__" $"
