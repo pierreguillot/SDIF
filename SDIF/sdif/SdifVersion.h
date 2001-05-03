298107b4c45d208583bf33dabf43f34b6b928a95
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2001-05-03 13:02:13 +0000

    make use of new SdifTypes version 1.6 for compilation/instalation

--- SDIF/sdif/SdifVersion.h
+++ SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$SDIFChecksum: 2971497847 $ IRCAM $SDIFdate: "__DATE__" $"
+#define AUTOCKSUM "$SDIFChecksum: 3709908093 $ IRCAM $SDIFdate: "__DATE__" $"
