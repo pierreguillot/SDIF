e562e2d8a4fa6493749df8e0aff0c005aacfc4de
nobody <nobody@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-04-11 16:13:32 +0000

    This commit was manufactured by cvs2svn to create tag 'V1'.

--- SDIF/include/sdif_version.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SDIF_VERSION_H
-#define _SDIF_VERSION_H 1
-
-#define SDIF_VERSION_MAJOR 3 
-#define SDIF_VERSION_MINOR 5 
-
-#define TOSTRING(x) #x
-
-#endif
--- SDIF/sdif/SdifVersion.h
+++
@@ -1,2 +0,0 @@
-/* Automatically generated File do not edit */
-#define AUTOCKSUM "$SDIFChecksum: 3781042799 $ IRCAM $SDIFdate: "__DATE__" $"
