fe848c2810ab8e73248963751f3988c34dd1fdc0
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-08-05 14:32:04 +0000

    Version 3.6

--- SDIF/sdif/SdifVersion.h
+++ SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$SDIFChecksum: 1524643014 $ IRCAM $SDIFdate: "__DATE__" $"
+#define AUTOCKSUM "$SDIFChecksum: 424652053 $ IRCAM $SDIFdate: "__DATE__" $"
