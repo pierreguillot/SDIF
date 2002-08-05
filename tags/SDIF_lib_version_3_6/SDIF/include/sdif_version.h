360cf056cb579de87aff9f9ea8c319e211dac4a6
nobody <nobody@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-08-05 15:38:22 +0000

    This commit was manufactured by cvs2svn to create tag 'SDIF_lib_version_3_6'.

---
+++ tags/SDIF_lib_version_3_6/SDIF/include/sdif_version.h
@@ -0,0 +1,9 @@
+#ifndef _SDIF_VERSION_H
+#define _SDIF_VERSION_H 1
+
+#define SDIF_VERSION_MAJOR 3 
+#define SDIF_VERSION_MINOR 6 
+
+#define TOSTRING(x) #x
+
+#endif
---
+++ tags/SDIF_lib_version_3_6/SDIF/sdif/SdifVersion.h
@@ -0,0 +1,2 @@
+/* Automatically generated File do not edit */
+#define AUTOCKSUM "$SDIFChecksum: 424652053 $ IRCAM $SDIFdate: "__DATE__" $"
