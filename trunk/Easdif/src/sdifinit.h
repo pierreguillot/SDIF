d32587721bb50c036fa77dc93a821f1687e850c2
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-08-28 16:45:41 +0000

    Initial Sdif Version based on the work Fabien Tisseran with some name changes.

---
+++ trunk/Easdif/src/sdifinit.h
@@ -0,0 +1,17 @@
+#ifndef SDIFINIT_H_
+#define SDIFINIT_H_ 1
+
+#include <sdif.h>
+#include "easdif/err_excep.hpp"
+
+/*
+  Initialise the SDIF library, providing a name for an optional additional
+  file with type definitions or "". And install exceptions.
+  <b>This function has to be called once and only once per process 
+  before any other call to the SDIF library.</b> 
+*/
+void EasdifInit(std::string PredefinedType = "");
+
+void EasdifEnd();
+
+#endif
