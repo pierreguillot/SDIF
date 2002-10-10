23e345e721e9c0a5c6f6f23b162d076309810a01
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-10-10 10:50:07 +0000

    Adapted to use namespace and new eof handling.

--- Easdif/test/tutofini.cpp
+++ Easdif/test/tutofini.cpp
@@ -1,5 +1,6 @@
 #include "easdif/easdif.h"
 
+using namespace Easdif;
 
 int main(int argc, char** argv)
 {  
@@ -68,93 +69,95 @@ int main(int argc, char** argv)
     /******** READ AND WRITE  ********/
     /* to show the exception : we break the loop with an exception */
     try{
+      while (1)	{
+
 	/* reading the next frame of the EntityRead, return the number of
 	 * bytes read, return 0 if the frame is not selected and -1 if this is 
 	 * the last frame of the file */
-        while ((i=readentity.ReadNextFrame(frame)) != -1)
-	{
-	    /* the next part of the loop is for writing just the selected 
-	     * frames. If the current frame is not selected we go to the
-	     * next frame */
-	    if (i == 0)
-		continue;
-       	        
-	    /* for adding new matrix in EntityToWrite */
-	    for (unsigned int i=0 ; i < frame.GetNbMatrix() ; i++)
-	    {
-		/*take the matrix number "i" and put it in tmpMatrix */
-		SDIFMatrix tmpMatrix = frame.GetMatrix(i);
-	 
-		/*add a matrix in the matrix vector of the frame*/
-		frameTowrite.AddMatrix(tmpMatrix);
-		/*an other method to add just the frame selected in file:
-		 * frameTowrite.AddMatrixSelected(readentity.GetFile(),
-		 * tmpMatrix);*/
-
-		/* if you want to access to the data : an example, if we want 
-		 * to multiply with 2 the last column of a matrix : */
-		double dou;
-		int ncols = tmpMatrix.GetNbCols();
-		for(int i = 0 ; i < tmpMatrix.GetNbRows() ; i++)
-		{
-		    //dou = tmpMatrix.GetDouble(i, ncols-1);
-		    /* to get the value in the double dou*/
-		    tmpMatrix.Get(i, ncols-1,dou);
-		    /* an other method can be :
-		       dou = tmpMatrix.GetDouble(i, ncols-1);
-		    */
-		    tmpMatrix.Set(i, ncols-1,2.*dou);
-		}
-	    }
+	if(!readentity.ReadNextFrame(frame))	    
+	  continue;
 	
-	    /* to set the header of the new frame with the one of a frame :*/
-	    frameTowrite.SetInfo(frame.GetSignature(),
-				 frame.GetStreamID(),
-				 frame.GetTime() );
+	/* for adding new matrix in EntityToWrite */
+	for (unsigned int i=0 ; i < frame.GetNbMatrix() ; i++)
+	  {
+	    /*take the matrix number "i" and put it in tmpMatrix */
+	    SDIFMatrix tmpMatrix = frame.GetMatrix(i);
+	    
+	    /*add a matrix in the matrix vector of the frame*/
+	    frameTowrite.AddMatrix(tmpMatrix);
+	    /*an other method to add just the frame selected in file:
+	     * frameTowrite.AddMatrixSelected(readentity.GetFile(),
+	     * tmpMatrix);*/
+	    
+	    /* if you want to access to the data : an example, if we want 
+	     * to multiply with 2 the last column of a matrix : */
+	    double dou;
+	    int ncols = tmpMatrix.GetNbCols();
+	    for(int i = 0 ; i < tmpMatrix.GetNbRows() ; i++)
+	      {
+		//dou = tmpMatrix.GetDouble(i, ncols-1);
+		/* to get the value in the double dou*/
+		tmpMatrix.Get(i, ncols-1,dou);
+		/* an other method can be :
+		   dou = tmpMatrix.GetDouble(i, ncols-1);
+		*/
+		tmpMatrix.Set(i, ncols-1,2.*dou);
+	      }
+	  }
 	
-	    /* an other method can be :
-	       frameTowrite.SetStreamID(frame.GetStreamID());
-	       frameTowrite.SetTime(frame.GetTime());
-	       frameTowrite.SetSignature(frame.GetSignature());
-	    */
-
-	    /* to write the EntityToWrite */
-	    frameTowrite.Write(entity);
-
-	    /* for reading a frame of the EntityToWrite, this make an 
-	     * exception because the file is at end*/
-	    //frame.Read(entity);
-
-	    /* clean the frames */
-	    frameTowrite.ClearData();
-	    frame.ClearData();
-	}
+	/* to set the header of the new frame with the one of a frame :*/
+	frameTowrite.SetInfo(frame.GetSignature(),
+			     frame.GetStreamID(),
+			     frame.GetTime() );
+	
+	/* an other method can be :
+	   frameTowrite.SetStreamID(frame.GetStreamID());
+	   frameTowrite.SetTime(frame.GetTime());
+	   frameTowrite.SetSignature(frame.GetSignature());
+	*/
+	
+	/* to write the EntityToWrite */
+	frameTowrite.Write(entity);
+	
+	/* for reading a frame of the EntityToWrite, this make an 
+	 * exception because the file is at end*/
+	//frame.Read(entity);
+	
+	/* clean the frames */
+	frameTowrite.ClearData();
+	frame.ClearData();
+      }
     }
-
+    
     /* to catch an exception */
     catch(SDIFEof& e)
-    {
+      {
 	std::cerr << " Catch EOF " << std::endl;
 	/* if we want an access to the file */
 	SdifFileT *sf = e.sdifFile();
 	/* to have the error message */
 	e.ErrorMessage();
-    }
-
+      }
+    
     catch(SDIFUnDefined& e)
-    {
+      {
 	std::cerr << " Catch Undefined " << std::endl;	
 	e.ErrorMessage();
-    }
-
-    catch(SDIFException&e)
-    {
-	std::cerr << " Catch other Exception " << std::endl;
+      }
+    
+    catch(Easdif::SDIFException&e)
+      {
+	std::cerr << " Catch other SDIFException " << std::endl;
 	e.ErrorMessage();
-    }
+      }
+    catch(std::exception &e)
+      {
+	std::cerr << " Catch other Exception: " << e.what() <<std::endl;
 
+      }
+    
     /* deinitialise the SDIF library */
     EasdifEnd();
     return 0;
 }
-   
+
