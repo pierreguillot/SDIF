40fc66856f82033d39d7d9c1cc56a9b908307c17
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-11-07 21:04:28 +0000

    Changed Set return value to void. Added method to clone data.

--- Easdif/src/sdifmatrixdatainterface.h
+++ Easdif/src/sdifmatrixdatainterface.h
@@ -8,9 +8,14 @@
  * 
  * 
  * 
- * $Id: sdifmatrixdatainterface.h,v 1.4 2002-10-10 10:49:09 roebel Exp $ 
+ * $Id: sdifmatrixdatainterface.h,v 1.5 2002-11-07 21:04:28 roebel Exp $ 
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2002/10/10 10:49:09  roebel
+ * Now using namespace Easdif.
+ * Fixed handling of zero pointer arguments in initException.
+ * Reading past end of file now throws an exception.
+ *
  * Revision 1.3  2002/08/28 16:46:53  roebel
  * Internal reorganization and name changes.
  *
@@ -51,9 +56,11 @@ public:
     virtual void   Resize(int nrows, int ncols)=0;
     virtual int    write(SdifFileT* file)=0;
 
-    virtual int    Set(int row, int col, const int& value)=0;
-    virtual int    Set(int row, int col, const float& value)=0;
-    virtual int    Set(int row, int col, const double& value)=0;
+    virtual void   Set(int row, int col, const int& value)=0;
+    virtual void   Set(int row, int col, const float& value)=0;
+    virtual void   Set(int row, int col, const double& value)=0;
+    
+  virtual SDIFMatrixDataInterface * clone()=0;
 
     virtual ~SDIFMatrixDataInterface() {};
 };
