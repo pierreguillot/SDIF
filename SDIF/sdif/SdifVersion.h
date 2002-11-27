e69707e601d7ea3b598363aa56e7aaca7cbc6286
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-11-27 17:55:13 +0000

    support for enable-ftruncate.

--- SDIF/sdif/SdifVersion.h
+++ SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$SDIFChecksum: 3593623114 $ IRCAM $SDIFdate: "__DATE__" $"
+#define AUTOCKSUM "$SDIFChecksum: 1416425939 $ IRCAM $SDIFdate: "__DATE__" $"
