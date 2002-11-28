c46a8173d4952d02c1c92c929dbf3db717903e08
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-11-28 19:58:49 +0000

    Fixed some const arguments. Make SdifFtruncte return SDIF_FTRUNCATE_NOT_AVAILABLE if this is the case. Removed redundant declarartions from header files.

--- trunk/SDIF/sdif/SdifVersion.h
+++ trunk/SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$SDIFChecksum: 1416425939 $ IRCAM $SDIFdate: "__DATE__" $"
+#define AUTOCKSUM "$SDIFChecksum: 2188251834 $ IRCAM $SDIFdate: "__DATE__" $"
