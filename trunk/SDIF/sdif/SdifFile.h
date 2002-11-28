74f83cb9cc28460b34881c621ae9e8e68afd84a0
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-11-28 19:57:19 +0000

    Removed redunant declarations that are already in sdif.h.

--- trunk/SDIF/sdif/SdifFile.h
+++ trunk/SDIF/sdif/SdifFile.h
@@ -1,4 +1,4 @@
-/* $Id: SdifFile.h,v 3.15 2001-05-02 09:34:43 tisseran Exp $
+/* $Id: SdifFile.h,v 3.16 2002-11-28 19:57:11 roebel Exp $
  *
  * IRCAM SDIF Library (http://www.ircam.fr/sdif)
  *
@@ -34,6 +34,9 @@ LIBRARY
 
 LOG
  * $Log: not supported by cvs2svn $
+ * Revision 3.15  2001/05/02 09:34:43  tisseran
+ * Change License from GNU Public License to GNU Lesser Public License.
+ *
  * Revision 3.14  2001/04/20 14:04:07  tisseran
  * SDIF VERSION 3.3
  * Create function: SdifTakeCodedPredefinedTypesfromString (SdifFile.[hc])
@@ -141,230 +144,6 @@ LOG
 
 #include "SdifGlobals.h"
 
-
-/*
-// FUNCTION GROUP:	Opening and Closing of Files
-*/
-
-/*DOC:
- */
-SdifFileT*         SdifFOpen                    (const char *Name, SdifFileModeET Mode);
-SdifFileT*         SdifOpenFile                 (const char *Name, SdifFileModeET Mode);
-SdifFileT*         SdifFOpenText                (SdifFileT *SdifF, const char* Name, SdifFileModeET Mode);
-/*DOC:
- */
-void               SdifFClose                   (SdifFileT *SdifF);
-void               SdifCloseFile                (SdifFileT *SdifF);
-SdifFrameHeaderT*  SdifFCreateCurrFramH         (SdifFileT *SdifF, SdifSignature Signature);
-SdifMatrixHeaderT* SdifFCreateCurrMtrxH         (SdifFileT *SdifF);
-FILE*              SdifFGetFILE_SwitchVerbose   (SdifFileT *SdifF, int Verbose);
-void               SdifTakeCodedPredefinedTypes (SdifFileT *SdifF);
-void               SdifTakeCodedPredefinedTypesfromString(SdifFileT *SdifF);
-void               SdifFLoadPredefinedTypes     (SdifFileT *SdifF, char *TypesFileName);
-
-extern int	  gSdifInitialised;
-extern SdifFileT *gSdifPredefinedTypes;
-
-
-
-/*
-// FUNCTION GROUP:	Init/Deinit of the Library
-*/
-
-/*DOC: 
-  Initialise the SDIF library, providing a name for an optional additional
-  file with type definitions or "".
-  <b>This function has to be called once and only once per process 
-  before any other call to the SDIF library.</b> */
-void SdifGenInit (char *PredefinedTypesFile); 
-
-/*DOC:
-  Initialise the SDIF library if it has not been initialised before.
-  This function has to be called at least once, but can be called as
-  many times as desired.  Especially useful for dynamic libraries.
-
-  [in] PredefinedTypesFile:
-	name for an optional additional file with type definitions or "". */
-void SdifGenInitCond (char *PredefinedTypesFile);
-
-/*DOC:
-  Deinitialise the SDIF library */
-void SdifGenKill (void); 
-
-/*DOC:
-  Print version information to standard error. */
-void SdifPrintVersion(void);
-
-
-/*
-// FUNCTION GROUP:	Current Header Access Functions
-*/
-
-/*DOC: 
-  Permet de donner des valeurs à chaque champ de l'entête de frame
-  temporaire de SdifF.<p> 
-
-  Exemple:
-  <code>SdifSetCurrFrameHeader(SdifF, '1FOB', _SdifUnknownSize, 3, streamid, 1.0);</code> */
-SdifFrameHeaderT* SdifFSetCurrFrameHeader (SdifFileT *SdifF, 
-					   SdifSignature Signature, 
-					   SdifUInt4 Size,
-					   SdifUInt4 NbMatrix, 
-					   SdifUInt4 NumID, 
-					   SdifFloat8 Time);
-
-/*DOC: 
-  Permet de donner des valeurs à chaque champ de l'entête de matice
-  temporaire de SdifF.<p>
-
-  Exemple:
-  <code>SdifSetCurrMatrixHeader(SdifF, '1FOF', eFloat4, NbFofs, 7);</code> */
-SdifMatrixHeaderT* SdifFSetCurrMatrixHeader (SdifFileT *SdifF, 
-					     SdifSignature Signature,
-					     SdifDataTypeET DataType, 
-					     SdifUInt4 NbRow, SdifUInt4 NbCol);
-
-
-/*DOC: 
-  Recopie la mémoire pointée par Values en fonction de l'entête de
-  matrice courante.<p> 
-
-  Exemple:<br>
-<pre>
-  #define NbCols = 10;<br>
-
-  float t[NbCols] = { 1., 2., 3., 4., 5., 6., 7., 8., 9., 0.};<br>
-
-  SdifFSetCurrMatrixHeader(SdifF, 'mtrx', eFloat4, 1, NbCols);<br>
-  SdifFSetCurrOneRow      (SdifF, (void*) t);<br>
-</pre>
-
-  On connait la taille de la mémoire à recopier par le type de donnée
-  (ici: eFloat4) et le nombre de colonnes (ici: NbCols). Il faut que
-  le type de donnée de la matrice courante corresponde avec la taille
-  d'un élément de t. Si t est composé de float sur 4 bytes, alors on
-  doit avoir eFloat4. Si t est composé de double float sur 8 bytes,
-  alors c'est eFloat8.<br>
-
-  En général, les données d'un programme ne se présente pas sous cette
-  forme et il faut réaliser une transposition lors des transfert de
-  Sdif à un programme. Le programme Diphone Ircam a un bon exemple de
-  lecture avec transposition automatique, généralisée pour tout type
-  de matrice. */
-SdifOneRowT*  SdifFSetCurrOneRow       (SdifFileT *SdifF, void *Values);
-
-
-/*DOC: 
-  Permet de donner la valeur Value dans la ligbe de matrice temporaire
-  de SdifF à la colonne numCol (0<numCol<=SdifF->CurrMtrxH->NbCol).  */
-SdifOneRowT* SdifFSetCurrOneRowCol (SdifFileT *SdifF, SdifUInt4
-numCol, SdifFloat8 Value);
-
-
-/*DOC: 
-  Recupère la valeur stockée à la colonne numCol de la ligne
-  temporaire.  C'est un SdifFloat8 donc un double!!  */ 
-SdifFloat8 SdifFCurrOneRowCol (SdifFileT *SdifF, SdifUInt4 numCol);
-
-
-/*DOC: 
-  Idem que la fonction précédente mais en utilisant le type de la
-  matrice et le nom de la colonne.  */
-SdifFloat8    SdifFCurrOneRowColName   (SdifFileT *SdifF, 
-					SdifMatrixTypeT *MatrixType, 
-					char *NameCD);
-
-
-/*DOC: 
-  Renvoie la signature temporaire de Chunk ou de Frame.  */
-SdifSignature SdifFCurrSignature       (SdifFileT *SdifF);
-
-
-/*DOC: 
-  Met à 0 tous les bits de la signature temporaire.  */
-SdifSignature SdifFCleanCurrSignature  (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie la signature temporaire du dernier Frame lu ou du prochain à
-  écrire.  */
-SdifSignature SdifFCurrFrameSignature  (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie la signature temporaire de la dernier matrice lue ou de la
-  prochaine à écrire.  */
-SdifSignature SdifFCurrMatrixSignature (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie la ligne temporaire de SdifF.  */
-SdifOneRowT*  SdifFCurrOneRow          (SdifFileT *SdifF);
-
-/*DOC:
-  Returns a pointer to the data of the current matrix row.  According to the matrix data type, it can be a pointer to float or double. */
-void*	     SdifFCurrOneRowData	  (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie SdifF->CurrMtrx->NbCol, nombre de colonnes de la matrice en
-  cours de traitement.  */
-SdifUInt4     SdifFCurrNbCol           (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie SdifF->CurrMtrx->NbRow, nombre de lignes de la matrice en
-  cours de traitement.  */
-SdifUInt4     SdifFCurrNbRow           (SdifFileT *SdifF);
-
-/*DOC: 
-  Returns the data type of the current matrix. */
-SdifDataTypeET SdifFCurrDataType (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie SdifF->CurrFramH->NbMatrix, mombre de matrices du frame
-  courant.  */
-SdifUInt4     SdifFCurrNbMatrix        (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie SdifF->CurrFramH->NumID, index de l'objet du frame courant.  */
-SdifUInt4     SdifFCurrID              (SdifFileT *SdifF);
-
-/*DOC: 
-  Renvoie SdifF->CurrFramH->Time.  */
-SdifFloat8    SdifFCurrTime            (SdifFileT *SdifF);
-
-
-
-/*
-// FUNCTION GROUP:	File Data Access Functions
-*/
-
-/*DOC:
-  Return list of NVTs for querying. 
-  [] precondition NVTs have been read with SdifFReadAllASCIIChunks. */
-SdifNameValuesLT *SdifFNameValueList (SdifFileT *file);
-
-/*DOC:
-  Return number of NVTs present.
-  [] precondition NVTs have been read with SdifFReadAllASCIIChunks. */
-int SdifFNameValueNum (SdifFileT *file);
-
-/*DOC:
-  Return the file's stream ID table, created automatically by SdifFOpen. */
-SdifStreamIDTableT *SdifFStreamIDTable (SdifFileT *file);
-
-/*DOC:
-  Add user data, return index added */
-int SdifFAddUserData (SdifFileT *file, void *data);
-
-/*DOC:
-  Get user data by index */
-void *SdifFGetUserData (SdifFileT *file, int index);
-
-
-
-SdifFileT*    SdifFReInitMtrxUsed (SdifFileT *SdifF);
-SdifFileT*    SdifFPutInMtrxUsed  (SdifFileT *SdifF, SdifSignature Sign);
-SdifSignature SdifFIsInMtrxUsed   (SdifFileT *SdifF, SdifSignature Sign);
-
-
-
 /*
 // FUNCTION GROUP:	Error flag for file
 */
@@ -376,13 +155,5 @@ SdifSignature SdifFIsInMtrxUsed   (SdifFileT *SdifF, SdifSignature Sign);
   only warnings or remarks. */
 int SdifFNumErrors (SdifFileT *SdifF, SdifErrorLevelET upto);
 
-/*DOC: 
-  Return pointer to last error struct or NULL if no error present
-  for this file. */
-SdifErrorT*     SdifFLastError    (SdifFileT *SdifF);
-
-/*DOC: 
-  Return tag of last error or eNoError if no error present for this file. */
-SdifErrorTagET  SdifFLastErrorTag (SdifFileT *SdifF);
 
 #endif /* _SdifFile_ */
