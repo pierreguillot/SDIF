c097c087ca313104011ebf8014056166e1dfc654
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2002-12-13 16:32:21 +0000

    Bug fix in SdifNameValue

--- trunk/SDIF/sdif/SdifVersion.h
+++ trunk/SDIF/sdif/SdifVersion.h
@@ -1,2 +1,2 @@
 /* Automatically generated File do not edit */
-#define AUTOCKSUM "$SDIFChecksum: 2188251834 $ IRCAM $SDIFdate: "__DATE__" $"
+#define AUTOCKSUM "$SDIFChecksum: 2856336574 $ IRCAM $SDIFdate: "__DATE__" $"
