32e51dc0330f671226e2b052629d94648684998d
nobody <nobody@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-03-03 18:50:37 +0000

    This commit was manufactured by cvs2svn to create tag 'Easdif_lib_version_'.

---
+++ tags/Easdif_lib_version_/Easdif/autogen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+rm -f aclocal.m4 config.* install-sh libtool ltconfig ltmain.sh missing mkinstalldirs
+set -x
+
+#aclocal -I m4
+aclocal
+# On Mac OS X, libtoolize is glibtoolize
+libtoolize --force --automake --copy || glibtoolize --force --automake --copy
+autoconf
+automake --foreign --add-missing --copy
+ (
+     cd SDIF
+     ./autogen.sh
+ )
