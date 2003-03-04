a2f78ac757e9e242c94219d8103199b3359b3534
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-03-04 00:54:17 +0000

    version 0.30 (automaticly generated)

--- Easdif/easdif/easdif_version.h
+++ Easdif/easdif/easdif_version.h
@@ -1,6 +1,20 @@
-/* Version automatically generated from Makefile/DoxyFile
- * do not edit */
-#ifndef EASDIF_VERSION_MAJ
-#define EASDIF_VERSION_MAJ 0
-#define EASDIF_VERSION_MIN 22
-#endif 
+/* easdif/easdif_version.h.  Generated automatically by configure.  */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you have the <dlfcn.h> header file.  */
+#define HAVE_DLFCN_H 1
+
+/* Name of package */
+#define PACKAGE "Easdif"
+
+/* Version number of package */
+#define VERSION "."
+
+/* Major version number of package */
+#define EASDIF_VERSION_MAJ 0 
+
+/* minor version number of package */
+#define EASDIF_VERSION_MIN 30 
+
