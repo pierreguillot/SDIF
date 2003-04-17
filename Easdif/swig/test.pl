a5402fc6abb5291d939b560906a95e9d4a923d40
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-04-17 10:19:51 +0000

    Nice class and method names using renaming.

--- Easdif/swig/test.pl
+++ Easdif/swig/test.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
 
-# $Id: test.pl,v 1.2 2003-04-17 10:05:48 schwarz Exp $
+# $Id: test.pl,v 1.3 2003-04-17 10:19:51 schwarz Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.2  2003/04/17 10:05:48  schwarz
+# The same with shadow classes (= proper memory management and methods).
+#
 # Revision 1.1  2003/04/17 09:51:10  schwarz
 # First working swig generated perl extension!
 # Classes re-declared in easdif.i, simple method without shadow classes.
@@ -10,20 +13,19 @@
 
 use SDIF;
 
-$file = new SDIF::SDIFEntity;
+$file = new SDIF::Entity;
 print "created new SDIFEntity $file\n";
 
-$frame = new SDIF::SDIFFrame;
+$frame = new SDIF::Frame;
 print "created new SDIFFrame $frame\n";
 
 #with shadow class: 
-#$res = $file->SDIF::OpenRead("x");
-$res = $file->SDIF::SDIFEntity::OpenRead("../test/lic.sdif");
+$res = $file->SDIF::Entity::OpenRead("../test/lic.sdif");
 print "open...$res\n";
 
-while (!$file->SDIF::SDIFEntity::eof())
+while (!$file->SDIF::Entity::eof())
 {
-    $file->SDIF::SDIFEntity::ReadNextFrame($frame);
+    $file->SDIF::Entity::ReadNextFrame($frame);
     $count++;
 }
 
