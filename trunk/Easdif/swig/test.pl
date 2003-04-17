1957c7b6a1418cf9510378569a5b749375656012
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-04-17 10:05:48 +0000

    The same with shadow classes (= proper memory management and methods).

--- trunk/Easdif/swig/test.pl
+++ trunk/Easdif/swig/test.pl
@@ -1,26 +1,30 @@
 #!/usr/bin/perl
 
-# $Id: test.pl,v 1.1 2003-04-17 09:51:10 schwarz Exp $
+# $Id: test.pl,v 1.2 2003-04-17 10:05:48 schwarz Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.1  2003/04/17 09:51:10  schwarz
+# First working swig generated perl extension!
+# Classes re-declared in easdif.i, simple method without shadow classes.
+# Doesn't do much except counting frames.
 
 use SDIF;
 
-$file = SDIF::new_SDIFEntity;
+$file = new SDIF::SDIFEntity;
 print "created new SDIFEntity $file\n";
 
-$frame = SDIF::new_SDIFFrame;
+$frame = new SDIF::SDIFFrame;
 print "created new SDIFFrame $frame\n";
 
 #with shadow class: 
 #$res = $file->SDIF::OpenRead("x");
-$res = SDIF::SDIFEntity_OpenRead($file, "../test/lic.sdif");
+$res = $file->SDIF::SDIFEntity::OpenRead("../test/lic.sdif");
 print "open...$res\n";
 
-while (!SDIF::SDIFEntity_eof($file))
+while (!$file->SDIF::SDIFEntity::eof())
 {
-    SDIF::SDIFEntity_ReadNextFrame($file, $frame);
-      $count++;
+    $file->SDIF::SDIFEntity::ReadNextFrame($frame);
+    $count++;
 }
 
 print "found $count frames\n";
