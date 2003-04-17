65beb14b4b9df6ba234e32cdb72f2292792993ab
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-04-17 11:15:01 +0000

    Swigging sdif.h works (with some warnings), but that's not yet it...

--- trunk/Easdif/swig/test.pl
+++ trunk/Easdif/swig/test.pl
@@ -1,8 +1,12 @@
 #!/usr/bin/perl
 
-# $Id: test.pl,v 1.4 2003-04-17 11:03:52 schwarz Exp $
+# $Id: test.pl,v 1.5 2003-04-17 11:15:01 schwarz Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.4  2003/04/17 11:03:52  schwarz
+# Swigged all easdif classes!
+# Now going on to map sdif basic types.
+#
 # Revision 1.3  2003/04/17 10:19:51  schwarz
 # Nice class and method names using renaming.
 #
@@ -18,7 +22,7 @@ use SDIF;
 use Data::Dumper;
 
 $file = new SDIF::Entity;
-print "created new SDIFEntity $file\n", Dumper($file), "\nfields:\n", keys(%$file);
+print "created new SDIFEntity $file\n";
 
 
 $frame = new SDIF::Frame;
@@ -35,10 +39,15 @@ while (!$file->SDIF::Entity::eof())
     $res = $file->SDIF::Entity::ReadNextFrame($frame);
     $count{$frame->GetSignature}++;
 
+    # print frame to stdout
+    $frame->View();
+
     #print "reading frame $count: $res\n";
 }
 
+print $file->ViewTypes(), " types\n";
+
 while (my ($ft, $c) = each %count)
 {
-    print "found $c frames of type $ft\n";
+    print "found $c frames of type ", SDIF::SdifSignatureToString($ft), "\n";
 }
