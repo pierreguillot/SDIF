9d2763e935c060735ab5ee0b5d0b7a077ea29ddf
tisseran <tisseran@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-04-30 11:36:06 +0000

    Started autoconfiscation of swig directory

--- trunk/Easdif/autogen.sh
+++ trunk/Easdif/autogen.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# $Id: autogen.sh,v 1.3 2003-04-03 12:39:45 schwarz Exp $
+# $Id: autogen.sh,v 1.4 2003-04-30 11:36:06 tisseran Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.3  2003/04/03 12:39:45  schwarz
+# Added conditional to prevent endless loop when directory SDIF doesn't exist.
+#
 
 rm -f aclocal.m4 config.* install-sh libtool ltconfig ltmain.sh missing mkinstalldirs
 set -x
@@ -14,4 +17,11 @@ autoconf
 automake --foreign --add-missing --copy
 (
     cd SDIF  &&  ./autogen.sh
+)
+(
+    cd swig
+    aclocal
+    libtoolize --force --automake --copy || glibtoolize --force --automake --copy
+    autoconf
+    automake --foreign --add-missing --copy
 )
