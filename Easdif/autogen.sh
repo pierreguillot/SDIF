fd821436b3868f0eaafb34bbda2014d57ae359ef
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-08-07 16:28:45 +0000

    swig now has its own autogen.sh.

--- Easdif/autogen.sh
+++ Easdif/autogen.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# $Id: autogen.sh,v 1.5 2003-08-07 16:19:29 roebel Exp $
+# $Id: autogen.sh,v 1.6 2003-08-07 16:28:45 roebel Exp $
 #
 # $Log: not supported by cvs2svn $
+# Revision 1.5  2003/08/07 16:19:29  roebel
+# Java include directories are determined automatically using m4 functions. perl include directory are determined with improved flexibility.
+#
 # Revision 1.4  2003/04/30 11:36:06  tisseran
 # Started autoconfiscation of swig directory
 #
@@ -22,9 +25,5 @@ automake --foreign --add-missing --copy
     cd SDIF  &&  ./autogen.sh
 )
 (
-    cd swig
-    aclocal -I m4
-    libtoolize --force --automake --copy || glibtoolize --force --automake --copy
-    autoconf
-    automake --foreign --add-missing --copy
+    cd swig &&  ./autogen.sh
 )
