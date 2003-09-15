4a287c1641fadd5673ed0880dc4b609c32c6278a
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2003-09-15 17:06:58 +0000

    handle ::: correctly

--- sdif-matlab/sdifexist.m
+++ sdif-matlab/sdifexist.m
@@ -3,11 +3,14 @@
 %          sdifexist(name)	Exits with error if file's not there.
 % result = sdifexist(name)	Returns flag.
 
-% $Id: sdifexist.m,v 1.2 2000-05-12 14:03:52 schwarz Exp $
+% $Id: sdifexist.m,v 1.3 2003-09-15 17:06:58 schwarz Exp $
 %
 % sdifexist.m	4. May 2000	Diemo Schwarz
 %
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2000/05/12  14:03:52  schwarz
+% Oops-style errors.
+%
 % Revision 1.1  2000/05/04  13:24:08  schwarz
 % Matlab mex extension and support functions to load SDIF files.
 
@@ -15,11 +18,21 @@ function result = sdifexist (name)
     result = 1;
     
     % remove selection from filename
-    colon = find (name == ':');
-    if (length (colon) >= 2  &  colon(end-1) == colon(end)-1),
-	testname = name (1:colon(end-1)-1);	
-    else
-	testname = name;
+    colon  = findstr(name, '::');
+    
+    switch length(colon),
+     case 0,
+      testname = name;
+
+     case 1,
+      testname = name(1:colon-1);
+	
+     otherwise,
+      if colon(end-1) == colon(end)-1,
+	  testname = name(1:colon(end-1)-1);	% three consecutive :
+      else
+	  testname = name(1:colon(end)-1);	% separate groups of ::
+      end
     end
     
     % are you there?
