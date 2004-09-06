8d94c1fa0be3abae3e544ce80c38fb8dc4859ccd
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2004-09-06 16:50:58 +0000

    Fixed typo in USE_XPGUI macro.

--- SDIF/sdif/SdifConvToText.c
+++ SDIF/sdif/SdifConvToText.c
@@ -1,4 +1,4 @@
-/* $Id: SdifConvToText.c,v 3.11 2004-07-22 14:47:55 bogaards Exp $
+/* $Id: SdifConvToText.c,v 3.12 2004-09-06 16:50:58 roebel Exp $
  *
  * IRCAM SDIF Library (http://www.ircam.fr/sdif)
  *
@@ -30,6 +30,9 @@
  * author: Dominique Virolle 1997
  *
  * $Log: not supported by cvs2svn $
+ * Revision 3.11  2004/07/22 14:47:55  bogaards
+ * removed many global variables, moved some into the thread-safe SdifGlobals structure, added HAVE_PTHREAD define, reorganized the code for selection, made some arguments const, new version 3.8.6
+ *
  * Revision 3.10  2003/11/07 21:47:18  roebel
  * removed XpGuiCalls.h and replaced preinclude.h  by local files
  *
@@ -231,7 +234,7 @@ SdifFConvToTextAllFrame(SdifFileT *SdifF)
 	SizeR += SizeRSign;
 	SizeRSign = 0;
       }
-#ifdef USEXPGUI
+#ifdef USE_XPGUI
       {
 	long int fileSize = ftell(SdifF->Stream);
 	XpProBarSet((float)fileSize);
@@ -311,7 +314,7 @@ SdifToText(SdifFileT *SdifF, char *TextStreamName)
 	    }
       else
         {
-#ifdef USEXPGUI
+#ifdef USE_XPGUI
 	  {
 	    long int fileSize = XpFileSize(SdifF->Name);
 	    XpProBarString("Convert Sdif To Text");
