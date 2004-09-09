370fad713143967a1082a2e47dea2059bcec10d8
schwarz <schwarz@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2004-09-09 18:03:50 +0000

    completed with autoheader, display tools versions

--- SDIF/autogen.sh
+++ SDIF/autogen.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
-rm -f aclocal.m4 config.*  libtool ltconfig ltmain.sh missing mkinstalldirs
+
+# unset some stuff that disturbs the tools
+PERLLIB=
+PERL5LIB=
+PERLSITE=
+PERLARCH=
+export PERLLIB PERL5LIB PERLSITE PERLARCH
+
+# display versions
+echo "[auto-tools versions in your path on $HOST]"
+for f in aclocal automake autoconf autoheader autom4te libtoolize libtool; do
+    echo -n `$f --version | head -1` ": "
+    ls -lFgG `which $f`
+done
+
+rm -rf aclocal.m4 config.* install-sh libtool ltconfig ltmain.sh missing mkinstalldirs autom4te.cache
+
 set -x
-#aclocal -I m4
+
 aclocal
 # On Mac OS X, libtoolize is glibtoolize
 libtoolize --force --automake --copy || glibtoolize --force --automake --copy
+autoheader
 autoconf
 automake --foreign --add-missing --copy
-# (
-#     cd client
-#     aclocal
-#     autoconf
-#     automake --foreign --add-missing --copy
-# )
