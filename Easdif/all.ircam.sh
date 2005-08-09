5ed0e03c6e5262755b9c5e5f5521b5ff8202bd93
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2005-08-09 11:34:27 +0000

    adapted system dependend install script.

--- Easdif/all.ircam.sh
+++ Easdif/all.ircam.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
 ./autogen.sh
-./configure --prefix=/u/formes/share
+
+if [ `uname` = "Darwin" -o `uname` =  "Linux" ];then
+flags=$flags
+else
+flags="$flags CXXFLAGS=-mno-cygwin CFLAGS=-mno-cygwin LDFLAGS=-mno-cygwin"
+fi
+
+if [ "$1" = "" ]; then
+./configure $flags --prefix=/u/formes/share
+else
+./configure $flags --prefix="$1"
+fi
 make
 make install
