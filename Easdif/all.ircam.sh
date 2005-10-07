75f3c8a66db2207fd525d45a9cf320d8dea48916
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2005-10-07 20:06:58 +0000

    Use $SYS for bindir, too.

--- Easdif/all.ircam.sh
+++ Easdif/all.ircam.sh
@@ -9,9 +9,9 @@ flags="$flags CXXFLAGS=-mno-cygwin CFLAGS=-mno-cygwin LDFLAGS=-mno-cygwin"
 fi
 
 if [ "$1" = "" ]; then
-./configure $flags --prefix=/u/formes/share
+./configure $flags --prefix=/u/formes/share --libdir=/u/formes/share/lib/$SYS --bindir=/u/formes/share/bin/$SYS
 else
-./configure $flags --prefix="$1"
+./configure $flags --prefix="$1" --libdir="$1"/lib/$SYS --bindir="$1"/bin/$SYS
 fi
 make
 make install
