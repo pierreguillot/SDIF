56bb337ac32f33702d64459f6601a0fc8d6883b6
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2006-02-22 15:38:03 +0000

    Debug traget did not yet work, now it does

--- trunk/Easdif/all.ircam.sh
+++ trunk/Easdif/all.ircam.sh
@@ -11,9 +11,9 @@ export LDFLAGS="$LDFLAGS -mno-cygwin"
 fi
 set -x
 if [ "$1" = "" ]; then
-./configure --prefix=/u/formes/share --libdir=/u/formes/share/lib/$SYS --bindir=/u/formes/share/bin/$SYS
+./configure $configflags --prefix=/u/formes/share --libdir=/u/formes/share/lib/$SYS --bindir=/u/formes/share/bin/$SYS
 else
-./configure --prefix="$1" --libdir="$1"/lib/$SYS --bindir="$1"/bin/$SYS
+./configure $configflags --prefix="$1" --libdir="$1"/lib/$SYS --bindir="$1"/bin/$SYS
 fi
 make
 make install
