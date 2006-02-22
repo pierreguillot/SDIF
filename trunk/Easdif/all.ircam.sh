cc0d82026737c0bd899acc24ddcfc373499d5282
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2006-02-22 15:12:26 +0000

    Fixed debug target and use of SDKs and deployment targets

--- trunk/Easdif/all.ircam.sh
+++ trunk/Easdif/all.ircam.sh
@@ -5,13 +5,15 @@
 if [ `uname` = "Darwin" -o `uname` =  "Linux" ];then
 flags=$flags
 else
-flags="$flags CXXFLAGS=-mno-cygwin CFLAGS=-mno-cygwin LDFLAGS=-mno-cygwin"
+export CXXFLAGS="$CFLAGS -mno-cygwin" 
+export CFLAGS="$CFLAGS -mno-cygwin"
+export LDFLAGS="$LDFLAGS -mno-cygwin"
 fi
-
+set -x
 if [ "$1" = "" ]; then
-./configure $flags --prefix=/u/formes/share --libdir=/u/formes/share/lib/$SYS --bindir=/u/formes/share/bin/$SYS
+./configure --prefix=/u/formes/share --libdir=/u/formes/share/lib/$SYS --bindir=/u/formes/share/bin/$SYS
 else
-./configure $flags --prefix="$1" --libdir="$1"/lib/$SYS --bindir="$1"/bin/$SYS
+./configure --prefix="$1" --libdir="$1"/lib/$SYS --bindir="$1"/bin/$SYS
 fi
 make
 make install
