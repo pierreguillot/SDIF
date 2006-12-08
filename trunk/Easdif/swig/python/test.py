d99fe6e200b1ecb8415b9c40479ac20ba5942b8c
roebel <roebel@f0edb446-0c2a-49e4-ba95-239725f1d53d>
2006-12-08 18:02:20 +0000

    Extended python support for SelectionSet.

--- trunk/Easdif/swig/python/test.py
+++ trunk/Easdif/swig/python/test.py
@@ -11,8 +11,13 @@ frame = eaSDIF.Frame();
 print "created new SDIFFrame ", frame
 
 #with shadow class: 
-res = file.OpenRead("../../test/lic.sdif")
+res = file.OpenRead("/u/formes/roebel/src/EASDIF_SDIF/swig/python/../../test/mrk.sdif")
 print "open (mode ", eaSDIF.eReadFile , ")...",res
+
+set=eaSDIF.SelectionSet()
+set.insert(eaSDIF.CreateSignature('1','T','R','C'))
+
+file.RestrictMatrixSelection(set)
 
 count = {}
 valfsig = {}
@@ -21,35 +26,44 @@ valcount ={}
 valsum ={}
 valssum ={}
 while not file.eof():
-    res  = file.ReadNextFrame(frame);
-    fsig = frame.GetSignature();
-    # need typemap SdifSignature -> string 
-    #    count[fsig] += 1;
-
-    # print frame to stdout
-    #$frame->View();
-
-    mat  = frame.GetMatrix(0);
-    #    $nomat= $frame->GetMatrix(1);
-    msig = mat.GetSignature();
-    nrow = mat.GetNbRows();
-    ncol = mat.GetNbCols();
-    val  = mat.GetDouble(0, 0);
-    sig  = fsig + "/" + msig;
-
-    valfsig[sig]  = fsig
-    valmsig[sig]  = msig;
-    if valcount.has_key(sig):
-         valcount[sig] += 1
-         valsum[sig]   += val
-         valssum[sig]  += val * val
-    else:
-         valcount[sig] = 1
-         valsum[sig]   = val
-         valssum[sig]  = val * val
-         
-    print "frame " + fsig + " matrix " + msig + "(" + str(nrow) + ", " + str(ncol) + ") = " + str(val);
-
+    res  = file.ReadNextSelectedFrame(frame);
+    if res:
+        fsig = frame.GetSignature();
+        # need typemap SdifSignature -> string 
+        if count.has_key(fsig):
+            count[fsig] += 1;
+        else:
+            count[fsig] = 1;
+        
+        # print frame to stdout
+        # frame.Print();
+        if frame.GetNbMatrix() > 0 :
+            mat  = frame.GetMatrix(0);
+            #    $nomat= $frame->GetMatrix(1);
+            msig = mat.GetSignature();
+            nrow = mat.GetNbRows();
+            ncol = mat.GetNbCols();
+            if nrow > 0  and ncol > 0 :
+                val  = mat.GetDouble(0, 0);
+            else:
+                val = 0;
+                
+            sig  = fsig + "/" + msig;
+                
+            valfsig[sig]  = fsig
+            valmsig[sig]  = msig;
+            if valcount.has_key(sig):
+                valcount[sig] += 1
+                valsum[sig]   += val
+                valssum[sig]  += val * val
+            else:
+                valcount[sig] = 1
+                valsum[sig]   = val
+                valssum[sig]  = val * val
+                
+            print "frame " + fsig + " matrix " + msig + "(" + str(nrow) + ", " + str(ncol) + "), val(0,0) = " + str(val);
+        else :
+            print "matrix skipped"
 
 print "\ntypes defined in file:\n";
 file.PrintTypes();
